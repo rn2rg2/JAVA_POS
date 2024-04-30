8a8cdd5566b9d8460cf14073beaa875b16821933
rn2rong2 <koon11165@gmail.com>
2024-04-30 15:40:09 +0900

    dd

---
+++ src/com/kosa/dbconnection/JdbcConnection.java
@@ -0,0 +1,23 @@
+package com.kosa.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import oracle.jdbc.pool.OracleDataSource;
+
+public class JdbcConnection {
+
+	@SuppressWarnings("unused")
+	public static void main(String args[]) throws SQLException {
+
+		OracleDataSource ods = new OracleDataSource();
+		/* 설정 파일 + 싱글턴 패턴 활용 접속 */
+		Connection conn5 = DBConnection.getConnection();
+		System.out.println(conn5);
+		/* Oracle Call Interface (OCI) driver */
+
+		
+	}
+
+}
